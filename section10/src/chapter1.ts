265ad19710c25483f92e17890d70349f3a906f61
이아름 <areum6321@gmail.com>
2024-12-14 01:16:40 +0900

    TypeScript 맵드 타입 기반의 유틸리티 타입(Partial, Required, Readonly)

---
+++ section10/src/chapter1.ts
@@ -0,0 +1,58 @@
+/**
+ * Partial<T>
+ * -> 부분적인, 일부분의
+ * -> 특정 객체 타입의 모든 프로퍼티를 선택적 프로퍼티로 바꿔주는 타입
+ */
+
+import { title } from "process";
+
+interface Post {
+  title: string;
+  tags: string[];
+  content: string;
+  thumbnailURL?: string;
+}
+
+type Partial<T> = {
+  [key in keyof T]?: T[key];
+};
+
+const draft: Partial<Post> = {
+  title: "제목",
+  content: "초안...",
+};
+
+/**
+ * Required<T>
+ * -> 필수의, 필수적인
+ * -> 특정 객체 타입의 모든 프로퍼티를 필수 프로퍼티로 바꿔주는 타입
+ */
+
+type Required<T> = {
+  [key in keyof T]-?: T[key];
+};
+
+const withThumbnailPost: Required<Post> = {
+  title: "한입 ts 후기",
+  tags: ["ts"],
+  content: "",
+  thumbnailURL: "https://...",
+};
+
+/**
+ * Readonly<T>
+ * -> 읽기 전용
+ * -> 특정 객체 타입에서 모든 프로퍼티를 읽기 전용 프로퍼티로 만들어주는 타입
+ */
+
+type Readonly<T> = {
+  readonly [key in keyof T]: T[key];
+};
+
+const ReadonlyPost: Readonly<Post> = {
+  title: "보호된 게시글 입니다.",
+  tags: [],
+  content: "",
+};
+
+// ReadonlyPost.content = "";
