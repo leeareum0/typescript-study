f4c7b37365955049c29eacc1e317dd779e178cae
이아름 <areum6321@gmail.com>
2024-12-14 00:11:45 +0900

    TypeScript 원시 타입과 리터럴 타입

---
+++ section02/src/chapter01.ts
@@ -0,0 +1,29 @@
+// Number
+let num1: Number = 123;
+let num2: Number = -123;
+let num3: Number = 0.123;
+let num4: Number = -0.123;
+let num5: Number = Infinity;
+let num6: Number = -Infinity;
+let num7: Number = NaN;
+
+// String
+let str1: String = "hello";
+let str2: String = "hello";
+let str3: String = `hello`;
+let str4: String = `hello ${num1}`;
+
+// boolean
+let bool1: boolean = true;
+let bool2: boolean = false;
+
+// null
+let null1: null = null;
+
+// undefined
+let unde1: undefined = undefined;
+
+// 리터럴 타입
+// -> 타입에 값을 바로 입력한다.
+let numA: 10 = 10;
+let strA: "hello" = "hello";
